7dd17e2ba50f346f69d89f2cfa5b2bbe4cf5fdf0
Seonghoon Lee <82031787+shoon0123@users.noreply.github.com>
2025-07-06 15:05:39 +0900

    [Gold IV] Title: 사이클 게임, Time: 180 ms, Memory: 9712 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+static std::vector<int> parent;
+
+int find(int a)
+{
+    return a == parent[a] ? a : parent[a] = find(parent[a]);
+}
+
+void unionfunc(int a, int b)
+{
+    parent[find(a)] = find(b);
+}
+
+bool isSameUnion(int a, int b)
+{
+    return find(a) == find(b);
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int n, m;
+    std::cin >> n >> m;
+    parent.resize(n + 1);
+
+    for (int i = 1; i <= n; ++i)
+    {
+        parent[i] = i;
+    }
+
+    int dot1, dot2;
+    int endRound = 0;
+    for (int i = 1; i <= m; ++i)
+    {
+        std::cin >> dot1 >> dot2;
+        if (endRound == 0)
+        {
+            if (isSameUnion(dot1, dot2))
+            {
+                endRound = i;
+                continue;
+            }
+            else
+            {
+                unionfunc(dot1, dot2);
+            }
+        }
+    }
+    std::cout << endRound;
+}
