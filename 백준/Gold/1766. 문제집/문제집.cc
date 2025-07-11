427735ace82e82dde02a1b4e0e05bffb6f3b2ce8
Seonghoon Lee <82031787+shoon0123@users.noreply.github.com>
2025-07-11 23:46:21 +0900

    [Gold II] Title: 문제집, Time: 24 ms, Memory: 3944 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.cc
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <queue>
 int main()
 {
     std::ios::sync_with_stdio(false);
@@ -12,7 +12,6 @@ int main()
 
     std::vector<std::vector<int>> edges(n + 1);
     std::vector<int> indegree(n + 1);
-    std::vector<bool> visited(n + 1);
 
     int start, end;
     while (m--)
@@ -21,17 +20,28 @@ int main()
         edges[start].push_back(end);
         ++indegree[end];
     }
+
+    std::priority_queue<int,std::vector<int>,std::greater<int>> q;
     for (int i = 1; i <= n; ++i)
     {
-        if (indegree[i] == 0 && !visited[i])
+        if (indegree[i] == 0)
         {
-            std::cout << i << " ";
-            visited[i] = true;
-            for (const int& nextNode : edges[i])
+            q.push(i);
+        }
+    }
+
+    int top;
+    while (!q.empty())
+    {
+        top = q.top();
+        q.pop();
+        std::cout << top << " ";
+        for (const int& nextNode : edges[top])
+        {
+            if (--indegree[nextNode] == 0)
             {
-                --indegree[nextNode];
+                q.push(nextNode);
             }
-            i = 0;
         }
     }
 }
