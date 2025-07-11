1cbc75d6e50d4dea06324d5badc233cdd24ea33c
Seonghoon Lee <82031787+shoon0123@users.noreply.github.com>
2025-07-11 23:10:21 +0900

    [Gold III] Title: 줄 세우기, Time: 24 ms, Memory: 3944 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -8,32 +8,33 @@ int main()
     std::cin.tie(NULL);
     std::cout.tie(NULL);
 
-    int N, M;
+    static int N, M;
     std::cin >> N >> M;
-    std::vector<std::vector<int>> edges(N + 1);
-    std::vector<int> indegree(N + 1, 0);
+    std::vector<std::vector<int>> edges(N+1);
+    std::vector<int> indegree(N + 1);
 
-    while (M--) {
+    for (int i = 0; i < M; ++i) {
         int start, end;
         std::cin >> start >> end;
         edges[start].push_back(end);
-        ++indegree[end];
+        indegree[end]++;
     }
-
     std::queue<int> queue;
+
     for (int i = 1; i <= N; ++i) {
         if (indegree[i] == 0) {
             queue.push(i);
         }
     }
     while (!queue.empty()) {
-        int& now = queue.front();
+        int now = queue.front();
+        queue.pop();
         std::cout << now << " ";
-        for (const int& next : edges[now]) {
-            if (--indegree[next] == 0) {
+        for (int next : edges[now]) {
+            indegree[next]--;
+            if (indegree[next] == 0) {
                 queue.push(next);
             }
         }
-        queue.pop();
     }
 }
