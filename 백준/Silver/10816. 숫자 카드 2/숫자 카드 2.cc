501b96542401f17fbc0b4529a4306c04a8cae225
Seonghoon Lee <82031787+shoon0123@users.noreply.github.com>
2025-07-24 23:58:06 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 288 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,33 +1,33 @@
 #include <iostream>
-#include <unordered_map>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
     std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
+    std::cin.tie(nullptr);
 
-    int N, M;
+    int N;
     std::cin >> N;
-
-    std::unordered_map<int, int> countMap;
+    std::vector<int> cards(N);
 
     for (int i = 0; i < N; ++i)
-    {
-        int num;
-        std::cin >> num;
-        ++countMap[num];
-    }
+        std::cin >> cards[i];
 
+    std::sort(cards.begin(), cards.end());
+
+    int M;
     std::cin >> M;
 
     for (int i = 0; i < M; ++i)
     {
         int query;
         std::cin >> query;
-        std::cout << countMap[query] << " ";
+        auto lower = std::lower_bound(cards.begin(), cards.end(), query);
+        auto upper = std::upper_bound(cards.begin(), cards.end(), query);
+        std::cout << upper - lower << " ";
     }
-
     std::cout << '\n';
+
     return 0;
 }
