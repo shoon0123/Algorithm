1b99a6349ef88589b19fe961dd034ce0dcf35ecb
Seonghoon Lee <82031787+shoon0123@users.noreply.github.com>
2025-08-14 23:51:05 +0900

    [Silver IV] Title: 암기왕, Time: 1516 ms, Memory: 49016 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <set>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int T, N, M;
+    std::cin >> T;
+
+    std::set<int> intSet;
+    while (T--)
+    {
+        std::cin >> N;
+        while (N--)
+        {
+            int n;
+            std::cin >> n;
+            intSet.insert(n);
+        }
+        std::cin >> M;
+        while (M--)
+        {
+            int n;
+            std::cin >> n;
+            const int result = intSet.find(n) == intSet.end() ? 0 : 1;
+            std::cout << result << "\n";
+        }
+        intSet.clear();
+    }
+
+    return 0;
+}
