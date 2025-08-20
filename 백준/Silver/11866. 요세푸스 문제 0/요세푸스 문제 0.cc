39817453f69a35b6d4b6ae983e0c9e9e449805d8
Seonghoon Lee <82031787+shoon0123@users.noreply.github.com>
2025-08-20 22:14:50 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <queue>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int N, K;
+    std::cin >> N >> K;
+
+    std::queue<int> q;
+
+    for (int i = 1; i <= N; i++)
+    {
+        q.push(i);
+    }
+
+    std::cout << "<";
+
+    while (!q.empty())
+    {
+        for (int i = 0; i < K - 1; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+
+        std::cout << q.front();
+        q.pop();
+
+        if (!q.empty())
+        {
+            std::cout << ", ";
+        }
+    }
+
+    std::cout << ">\n";
+
+    return 0;
+}
